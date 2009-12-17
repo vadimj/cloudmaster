5e2f5d1dbb513ad36879789f6daed8602d477082
rabbitt <rabbitt@gmail.com>
2009-12-17 10:37:16 -0500

    tabs -> 2 spaces (simple consistency change)

--- lib/OriginalAWS/AWS.rb
+++ lib/OriginalAWS/AWS.rb
@@ -104,26 +104,26 @@ module AWS
 
   end
   
-    #
-    # Computes RFC 2104-compliant HMAC signature for request parameters
-    # Implements AWS Signature, as per following spec:
-    #
-    # If Signature Version is 0, it signs concatenated Action and Timestamp
-    #
-    # If Signature Version is 1, it performs the following:
-    #
-    # Sorts all  parameters (including SignatureVersion and excluding Signature,
-    # the value of which is being created), ignoring case.
-    #
-    # Iterate over the sorted list and append the parameter name (in original case)
-    # and then its value. It will not URL-encode the parameter values before
-    # constructing this string. There are no separators.
-    #
-    def signParameters(parameters, key, uri = nil, algorithm = :HmacSHA1 )
-        data = ""
-        signatureVersion = parameters['SignatureVersion']
+  #
+  # Computes RFC 2104-compliant HMAC signature for request parameters
+  # Implements AWS Signature, as per following spec:
+  #
+  # If Signature Version is 0, it signs concatenated Action and Timestamp
+  #
+  # If Signature Version is 1, it performs the following:
+  #
+  # Sorts all  parameters (including SignatureVersion and excluding Signature,
+  # the value of which is being created), ignoring case.
+  #
+  # Iterate over the sorted list and append the parameter name (in original case)
+  # and then its value. It will not URL-encode the parameter values before
+  # constructing this string. There are no separators.
+  #
+  def signParameters(parameters, key, uri = nil, algorithm = :HmacSHA1 )
+    data = ""
+    signatureVersion = parameters['SignatureVersion']
 
-        case signatureVersion.to_i
+    case signatureVersion.to_i
       when 0:
         data = calculateStringToSignV0(parameters)
       when 1:
@@ -134,23 +134,23 @@ module AWS
         data = calculateStringToSignV2(parameters, uri.is_a?(URI) ? uri : URI.parse(uri))
       else
         raise "Invalid Signature Version specified"
-        end
+      end
 
-        sign(data, key, algorithm)
-    end
+      sign(data, key, algorithm)
+  end
 
-    def calculateStringToSignV0(parameters)
-        parameters['Action']+parameters['Timestamp']
-    end
+  def calculateStringToSignV0(parameters)
+    parameters['Action']+parameters['Timestamp']
+  end
 
-    def calculateStringToSignV1(parameters)
-        parameters.sort {|x,y| x[0].downcase <=> y[0].downcase}.to_s
-    end
+  def calculateStringToSignV1(parameters)
+    parameters.sort {|x,y| x[0].downcase <=> y[0].downcase}.to_s
+  end
 
-    def calculateStringToSignV2(parameters, endpoint)
-        data = "POST\n"
-        data << endpoint.host.downcase + "\n"
-        data << endpoint.request_uri + "\n"
+  def calculateStringToSignV2(parameters, endpoint)
+    data = "POST\n"
+    data << endpoint.host.downcase + "\n"
+    data << endpoint.request_uri + "\n"
 
     # Sort, and encode parameters into a canonical string.
     sorted_params = parameters.sort {|x,y| x[0] <=> y[0]}
@@ -161,13 +161,13 @@ module AWS
     end
     data << encoded_params.join("&")
 
-        return data
-    end
+    return data
+  end
 
-    #
-    # Computes RFC 2104-compliant HMAC signature.
-    #
-    def sign(data, key, algorithm)
+  #
+  # Computes RFC 2104-compliant HMAC signature.
+  #
+  def sign(data, key, algorithm)
     case algorithm
       when :HmacSHA1
         digest_generator = OpenSSL::Digest::Digest.new('sha1')
@@ -175,11 +175,11 @@ module AWS
         digest_generator = OpenSSL::Digest::Digest.new('sha256')
       else
         raise "Non-supported signing method specified"
-        end
-
-        digest = OpenSSL::HMAC.digest(digest_generator, key, data)
-        encode_base64(digest)
     end
+
+    digest = OpenSSL::HMAC.digest(digest_generator, key, data)
+    encode_base64(digest)
+  end
 
   # Converts a minimal set of parameters destined for an AWS Query API
   # interface into a complete set necessary for invoking an AWS operation.
@@ -241,7 +241,7 @@ module AWS
 
     # Generate request description and signature, and add to the request
     # as the parameter 'Signature'
-  parameters['Signature'] = signParameters(parameters, @aws_secret_key, uri)
+    parameters['Signature'] = signParameters(parameters, @aws_secret_key, uri)
 
     case method
     when 'GET'
